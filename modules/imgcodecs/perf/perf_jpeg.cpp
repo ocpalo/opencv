9a0283cc9ffe0ebc9c602ea2f9324e324183b803
ocpalo <iamberkeyavas@gmail.com>
2022-09-11 21:25:24 +0300

    flag updates

--- modules/imgcodecs/perf/perf_jpeg.cpp
+++ modules/imgcodecs/perf/perf_jpeg.cpp
@@ -5,36 +5,34 @@
 
 namespace opencv_test
 {
-    using namespace perf;
+using namespace perf;
 
-    typedef perf::TestBaseWithParam<std::string> JPEG;
+PERF_TEST(JPEG, Decode)
+{
+    String filename = getDataPath("stitching/boat1.jpg");
 
-    PERF_TEST(JPEG, Decode)
-    {
-        String filename = getDataPath("stitching/boat1.jpg");
+    FILE *f = fopen(filename.c_str(), "rb");
+    fseek(f, 0, SEEK_END);
+    long len = ftell(f);
+    fseek(f, 0, SEEK_SET);
+    vector<uchar> file_buf((size_t)len);
+    EXPECT_EQ(len, (long)fread(&file_buf[0], 1, (size_t)len, f));
+    fclose(f); f = NULL;
 
-        FILE *f = fopen(filename.c_str(), "rb");
-        fseek(f, 0, SEEK_END);
-        long len = ftell(f);
-        fseek(f, 0, SEEK_SET);
-        vector<uchar> file_buf((size_t)len);
-        EXPECT_EQ(len, (long)fread(&file_buf[0], 1, (size_t)len, f));
-        fclose(f); f = NULL;
+    TEST_CYCLE() imdecode(file_buf, IMREAD_UNCHANGED);
 
-        TEST_CYCLE() imdecode(file_buf, IMREAD_UNCHANGED);
+    SANITY_CHECK_NOTHING();
+}
 
-        SANITY_CHECK_NOTHING();
-    }
+PERF_TEST(JPEG, Encode)
+{
+    String filename = getDataPath("stitching/boat1.jpg");
+    cv::Mat src = imread(filename);
 
-    PERF_TEST(JPEG, Encode)
-    {
-        String filename = getDataPath("stitching/boat1.jpg");
-        cv::Mat src = imread(filename);
+    vector<uchar> buf;
+    TEST_CYCLE() imencode(".jpg", src, buf);
 
-        vector<uchar> buf;
-        TEST_CYCLE() imencode(".jpg", src, buf);
-
-        SANITY_CHECK_NOTHING();
-    }
+    SANITY_CHECK_NOTHING();
+}
 
 } // namespace
